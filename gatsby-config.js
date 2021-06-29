1b5c2eb1aa99c4baa9c2b65eebc49b8716840548
brianpompey <brianthescholar@gmail.com>
2021-06-28 23:15:40 -0400

    changing the bio info

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Brian Pompey`,
+      summary: `who lives and works in NYC and is willing to relocate.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
