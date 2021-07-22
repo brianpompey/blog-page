da7d02ccd38b0ece430516d4556228f85491e4eb
brianpompey <brianthescholar@gmail.com>
2021-07-21 21:15:35 -0400

    adding input

--- src/components/search.js
+++ src/components/search.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const SearchBar = () => {
     <form action="/" method="get">
-        <label htmlFor="header-search"
+        <label htmlFor="header-search">
+            <span className="visually-hidden"> Search Blog Posts</span>
+        </label>
+        <input />
     </form>
 }
 
